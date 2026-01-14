ac05ca67f717b523dfdf509dd900a0808b1fa61b
TCCFCASTLE123 <clcastle00@gmail.com>
2026-01-13 17:42:24 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -57,15 +57,14 @@ function App() {
           localStorage.clear();
           setLoggedIn(false);
         }} />
-       <main className="app-main">
-  <Routes>
-    <Route path="/" element={<Inbox />} />
-    <Route path="/admin/templates" element={<AdminTemplates />} />
-    <Route path="/admin/templates/new" element={<TemplateFormPage />} />
-    <Route path="/admin/templates/:id" element={<TemplateFormPage />} />
-  </Routes>
-</main>
-
+        <main style={{ minHeight: "80vh" }}>
+          <Routes>
+            <Route path="/" element={<Inbox />} />
+            <Route path="/admin/templates" element={<AdminTemplates />} />
+            <Route path="/admin/templates/new" element={<TemplateFormPage />} />
+            <Route path="/admin/templates/:id" element={<TemplateFormPage />} />
+          </Routes>
+        </main>
       </div>
     </Router>
   );
