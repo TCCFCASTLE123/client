f1d6fb5ec6678946d2dc945001c6dd397ac006b2
TCCFCASTLE123 <clcastle00@gmail.com>
2026-02-24 15:07:43 -0700

    Update ConversationHeader.js

--- src/inbox/ConversationHeader.js
+++ src/inbox/ConversationHeader.js
@@ -1,1 +1,7 @@
-
+<div style={{ display: "flex", alignItems: "center", gap: 12, marginBottom: 10 }}>
+  {isMobile && ( ... )}
+  <h2>Conversation with {selectedClient.name}</h2>
+  <button>Edit</button>
+  <button>Delete</button>
+  <button>⋯</button>
+</div>
