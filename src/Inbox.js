00f0871a93b3b2d863d86704cfc4c2b1d5663e78
TCCFCASTLE123 <clcastle00@gmail.com>
2026-03-09 09:58:49 -0700

    Update Inbox.js

--- src/Inbox.js
+++ src/Inbox.js
@@ -5,7 +5,7 @@ import DetailsDrawer from "./inbox/DetailsDrawer";
 import ConversationHeader from "./inbox/ConversationHeader";
 import ConversationPanel from "./inbox/ConversationPanel";
 import { useLocation } from "react-router-dom";
-import api from "./services/api";
+import { api } from "../services/api";
 import { formatPhoneUS } from "./utils/phone";
 import { statusThemeByName } from "./utils/statusTheme";
 import ClientForm from "./inbox/ClientForm";
